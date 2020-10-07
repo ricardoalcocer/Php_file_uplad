1a6cddd164f7b645864693646af9de8cc62153a2
Ricardo Alcocer <ralcocer@gmail.com>
2020-10-07 12:49:13 -0500

    prepping for pushing

---
+++ README.md
@@ -0,0 +1,12 @@
+# PHP FILE UPLOADER WITH PROGRESS BAR
+
+![](https://drops.ricardoalcocer.com/drops/uploader_screenshot-cR1X1v8OvC.png)
+Yeah, so this is the simplest reusable PHP File Uploader I could come up with.
+
+# Usage
+
+Drop it on a PHP-enabled folder and open index.php.  It should work right ot of the box.
+
+# License
+
+MIT - alco.mit-license.org
--- index.php
+++ index.php
@@ -6,6 +6,10 @@
 
     $uploadFolder = "files/";
 
+    if (!is_dir($uploadFolder)) {
+        mkdir("$uploadFolder", 0777, true);
+    }
+
     if (!empty($_FILES['file'])){
         foreach ($_FILES['file']['name'] as $key => $name) {
             if($_FILES['file']['error'][$key] == 0 && move_uploaded_file($_FILES['file']['tmp_name'][$key],"{$uploadFolder}/{$name}")){
